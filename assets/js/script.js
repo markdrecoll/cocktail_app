2fa538e99a64f7fc1399db7d044356f58d0de36a
mdre797 <markdrecoll@gmail.com>
2021-04-12 14:41:57 -0500

    Worked on table to display drink data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+var drinkTableRowEl = document.getElementById('drinkTableRow');
+
 function getApi() {
-  var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka';
+  var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin';
 
   fetch(requestUrl)
     .then(function (response) {
@@ -8,6 +10,14 @@ function getApi() {
     .then(function (data) {
       //Using console.log to examine the data
       console.log(data);
-    });
-}
+    var tableColumn = document.createElement('td');
+    //   for (var i = 0; i < 5; i ++){
+    //     tableColumn.append(`${data.drinks[i].strDrink}`);
+    //   }
+    //   drinkTableRow.append(tableColumn);     
+    // });
+    }
+)};
+
 getApi();
+

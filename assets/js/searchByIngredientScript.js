a5628c440fb52eb1bbb6a740c6947eef0b5b135a
mdre797 <markdrecoll@gmail.com>
2021-04-14 11:33:54 -0500

    Buttons correctly displaying instructions to make drink.

--- assets/js/searchByIngredientScript.js
+++ assets/js/searchByIngredientScript.js
@@ -1,5 +1,24 @@
+
+//Here to line 12 triggers the modal with javascript
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+
+
+
+
 // get the drink table element from the html
+
 var drinkTableEl = document.getElementById('drinkTable');
+var modalTextEl = document.getElementById('modalText');
 
 function getApi(searchTerm) {
   // fetches data from the api, concatenates the user input for the ingredient called
@@ -23,7 +42,7 @@ function getApi(searchTerm) {
         // inside that table row, create an <td> and fill it with the data from the API
         tableRow.innerHTML = `<td> ${data.drinks[i].strDrink} </td> 
         <td> <img height="200" width="200" src="${data.drinks[i].strDrinkThumb}"</img></td>
-        <td><button class='detail-button' name='${data.drinks[i].idDrink}'>Get Details</button></td>`;
+        <td><button class='detail-button btn modal-trigger' data-target="modal1" name='${data.drinks[i].idDrink}'>Instructions</button></td>`;
         
         // write the above information into the actual page
         drinkTable.append(tableRow);
@@ -50,5 +69,8 @@ function getDrinkDetailApi(e){
     })
     .then(function (data) {
       console.log(data);
+      console.log(data.drinks[0].strInstructions);
+      modalText.append(data.drinks[0].strInstructions);
+      
     })
 }

93a8fcc9a1a7a695adf77a17ccdec3eb692162fa
mdre797 <markdrecoll@gmail.com>
2021-06-14 17:31:24 -0500

    Fixed for loops for random drink ingredient display.

--- assets/js/randomDrinkScript.js
+++ assets/js/randomDrinkScript.js
@@ -18,41 +18,43 @@ function getApi() {
         Glass: ${data.drinks[i].strGlass} <br>
         Category: ${data.drinks[i].strCategory}<br>
         <p class="instructionParagraphs">${data.drinks[i].strInstructions}</p><br><br>`);
+      
 
 
-      // // the api has the data as "strIngredient1" etc so
-      // // we make placeholder text here and add the number
-      // // of the current ingredient/measurement
-      // const ingredient_string = 'strIngredient';
-      // const measure_string = 'strMeasure'
-      // let drinkIngredients = [];
-      // let drinkMeasurements = [];
+      // the api has the data as "strIngredient1" etc so
+      // we make placeholder text here and add the number
+      // of the current ingredient/measurement
+      const ingredient_string = 'strIngredient';
+      const measure_string = 'strMeasure'
+      let drinkIngredients = [];
+      let drinkMeasurements = [];
 
-      // // for loop that cycles through the data and returns
-      // // every ingredient/measurement
-      // for(var i=1; i<= 15; i++){
-      //   var ingredientTemp = ingredient_string;
-      //   var measurementTemp= measure_string;
+      // for loop that cycles through the data and returns
+      // every ingredient/measurement
+      for(var j=1; j<= 15; j++){
+        var ingredientTemp = ingredient_string;
+        var measurementTemp= measure_string;
 
-      //   // makes the string "strIngredent" then whatever number you are on
-      //   ingredientTemp+=i;
-      //   measurementTemp+=i;
+        // makes the string "strIngredent" then whatever number you are on
+        ingredientTemp+=j;
+        measurementTemp+=j;
 
-      //   // if there is no ingredient/measurement then dont read in the data
-      //   if(data.drinks[0][ingredientTemp] !== null){
-      //     drinkIngredients.push(data.drinks[0][ingredientTemp]);
-      //   }
-      //   if(data.drinks[0][measurementTemp] !== null){
-      //     drinkMeasurements.push(data.drinks[0][measurementTemp]);
-      //   }
-      // }
-
-      // // for loop to append ingredients and measurements
-      // for(var i=0; i < drinkIngredients.length; i++){
-
-      //   randomDrinkDisplay01El.innerHTML += (`<p>${drinkMeasurements[i]} ${drinkIngredients[i]}</p>`)
-      // }
+        // if there is no ingredient/measurement then dont read in the data
+        if(data.drinks[0][ingredientTemp] !== null){
+          drinkIngredients.push(data.drinks[0][ingredientTemp]);
+        }
+        if(data.drinks[0][measurementTemp] !== null){
+          drinkMeasurements.push(data.drinks[0][measurementTemp]);
+        }
       }
+
+      // for loop to append ingredients and measurements
+      for(var k=0; k < drinkIngredients.length; k++){
+
+        randomDrinkDisplay01El.innerHTML += (`<p>${drinkMeasurements[k]} ${drinkIngredients[k]}</p>`)
+      }
+    }
+      
 
       for(var i=data.drinks.length/2;i<data.drinks.length; i++){
         randomDrinkDisplay02El.innerHTML += (`<h5>${data.drinks[i].strDrink}</h5>
